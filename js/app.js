e4350b2b9ed185feea85793b15345767dd86cb72
javohir <ajavohir969@gmail.com>
2024-08-25 07:53:06 +0500

    masala

---
+++ js/app.js
@@ -0,0 +1,94 @@
+{
+    let a = +prompt("Kvadrat tomonini kiriting");
+    b = 4 * a;
+    console.log(b);
+}
+
+
+// {
+//     let a = +prompt("Kvadrat tomonini kiriting");
+//     s = a * a;
+//     console.log(s);
+// }
+
+
+// {
+//     let a = +prompt("Turburchagni a tomoni kiriting");
+//     let b = +prompt("Turburchagni b tomoni kiriting");
+
+//     s = a * b;
+//     p = 2 * (a + b)
+//     console.log(s, p);
+// }
+
+
+// {
+//     let a = +prompt("Turburchagni a tomoni kiriting");
+//     let p = 3.14;
+//     let d
+//     d = a * p
+//     console.log(d);
+// }
+
+
+// {
+//     let a = +prompt("Aylananing deametri kiriting");
+//     let p = 3.14;
+//     let d
+//     d = a * p
+//     console.log(d);
+// }
+
+
+// {
+//     let a = +prompt("Kvadratni kiriting");
+//     let s = 6;
+//     a = a;
+//     s = s * (a * 2); 
+//     v = a * (a * a)
+//     console.log(v, s);
+// }
+
+
+// {
+//     let a = +prompt("Paralelepeped a  tomonini kiriting:");
+//     let b = +prompt("Paralelepeped b  tomonini kiriting:");
+//     let c = +prompt("Paralelepeped c  tomonini kiriting:");
+//     v = a * b * c;
+//     s = 2 * (a * b + b * c + a * c);
+//     console.log(v);
+//     console.log(s);
+// }
+
+
+// {
+//     let r = +prompt("Aylana radiusini kiriting");
+//     p = 3.14;
+//     l = 2 * r * p;
+//     s = p * (r * r);
+
+//     console.log("Aylananing uzunligi" + " " + l, "Aylananing yuzasi" + " " + s);
+// }
+
+
+// {
+//     let a = +prompt("Birinchi son")
+//     let b = +prompt("Ikkinchi son")
+//     const urta = (${(a + b) / 2})
+//     console.log(urta);
+// }
+
+
+// {
+//     let a = +prompt("Birinchi son")
+//     let b = +prompt("Ikkinchi son")
+//     const urta = (${(a * b) / 2})
+//     console.log(urta);
+// }
+
+
+// {
+//     let a = +prompt("Birinchi sonni kiriting");
+//     let b = +prompt("Ikkinchi sonni kiriting");
+//     console.log("Yig'indisi " + a + b, "Ko'paytmasi " + a * b, "Kvadrati " + a * a, "Kvadrati " + b * b);
+// }
